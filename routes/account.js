9804a6c017e43afc1db328ac3b0f2e02c61e58b7
Cristian <cmioveanu@gmail.com>
2021-04-12 11:22:00 +0100

    update login route

--- routes/account.js
+++ routes/account.js
@@ -9,60 +9,133 @@ const pool = new Pool(dbConfig);
 const passport = require('passport');
 const bcrypt = require('bcrypt');
 
+const checkAuth = require('../utils/checkAuth');
 
+account.post('/register', (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
 
-
-account.post('/join', async function (req, res) {
-    try {
-        const client = await pool.connect();
-        await client.query('BEGIN');
-        var pwd = await bcrypt.hash(req.body.password, 5);
-
-        await JSON.stringify(client.query('SELECT id FROM customers WHERE email=$1', [req.body.email], function (err, result) {
-            if (result.rows[0]) {
-                req.flash('warning', "This email address is already registered. < a href = '/login' > Log in !</a >");
-                res.redirect('/login');
-            }
-            else {
-                client.query('INSERT INTO customers (email, phone, username, password, first_name, last_name) VALUES ($1, $2, $3, $4, $5, $6)', [req.body.email, req.body.phone, req.body.username, pwd, req.body.firstName, req.body.lastName], function (err, result) {
-                    if (err) { console.log(err); }
-                    else {
-
-                        client.query('COMMIT')
-                        console.log(result)
-                        req.flash('success', 'User created.')
-                        res.redirect('/');
-                        return;
+    //if username exists, send 403 unauthorized; if not, create user and add it to the DB
+    pool.query(`
+    SELECT * FROM customers
+    WHERE email = $1`, [email], (err, result) => {
+        if (result.rows[0]) {
+            res.status(403).send("Username already exists. Please login.");
+        } else {
+            bcrypt.hash(password, 5, (err, hash) => {
+                pool.query(`
+                INSERT INTO customers (email, password)
+                VALUES ($1, $2)`, [email, hash], (err, result) => {
+                    if (err) {
+                        throw err;
                     }
+                    res.status(201).send("User registered.");
                 });
-            }
-
-        }));
-        client.release();
-    }
-    catch (e) { throw (e) }
+            });
+        }
+    });
 });
 
 
-
-account.post('/login', passport.authenticate('local', {
-    successRedirect: '/account',
-    failureRedirect: '/login',
-}),
-    function (req, res) {
-        if (req.body.remember) {
-            req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000; // Cookie expires after 30 days
-        } else {
-            req.session.cookie.expires = false; // Cookie expires at end of session
-        }
-        res.redirect('../');
-    });
+account.post('/login', passport.authenticate('local'), (req, res) => {
+    res.status(200).send("Logged in!");
+});
 
 
-
-account.get('/logout', function (req, res) {
+account.get('/logout', (req, res) => {
     req.logout();
-    res.redirect('../');
-    console.log("logged out!");
+    res.json({ status: "logout" });
+});
 
-}); 
+
+account.get('/checkLoginStatus', checkAuth, (req, res) => {
+    res.status(200).send('User is logged in.');
+});
+
+
+//change email
+account.put('/email', (req, res) => {
+    const password = req.body.password;
+    const newEmail = req.body.newEmail;
+
+    pool.query(`
+    SELECT password FROM customers
+    WHERE id = $1`, [req.user.id], (err, result) => {
+        if (err) {
+            throw err;
+        }
+
+        //password from the database
+        const DBPassword = result.rows[0].password;
+
+        //if submitted password and DB password match, update to the new email
+        bcrypt.compare(password, DBPassword, (err, check) => {
+            if (err) {
+                throw err;
+            }
+
+            if (check) {
+                pool.query(`
+                UPDATE customers
+                SET email = $1
+                WHERE id = $2`, [newEmail, req.user.id], (err, result) => {
+                    if (err) {
+                        throw err;
+                    }
+                    res.status(200).send('Email changed.');
+                });
+            } else {
+                res.status(403).send('Incorrect password.');
+            }
+        });
+
+    });
+});
+
+
+//change password
+account.put('/password', (req, res) => {
+    const oldPassword = req.body.oldPassword;
+    const newPassword = req.body.newPassword;
+
+
+    pool.query(`
+        SELECT password FROM customers
+        WHERE id = $1
+        `, [req.user.id], (err, result) => {
+        if (err) {
+            return done(err);
+        }
+
+        //password from the database
+        const DBPassword = result.rows[0].password;
+
+        //if old password and db password match, update to the new password
+        bcrypt.compare(oldPassword, DBPassword, (err, check) => {
+            if (err) {
+                throw err;
+            }
+
+            if (check) {
+                bcrypt.hash(newPassword, 5, (err, hash) => {
+                    if (err) {
+                        throw err;
+                    }
+
+                    pool.query(`
+                    UPDATE customers 
+                    SET password = $1
+                    WHERE id = $2
+                    `, [hash, req.user.id], (err, result) => {
+                        if (err) {
+                            throw err;
+                        }
+                        res.status(200).send("Password changed.");
+                    });
+                });
+            } else {
+                res.status(403).send("Passwords don't match.");
+            }
+        })
+    });
+});
