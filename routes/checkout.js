f85cd3775616c7f0068ef7fa6cdec8e593bed399
Cristian <cmioveanu@gmail.com>
2021-04-20 18:32:12 +0100

    integrate backend with actual products for checkout

--- routes/checkout.js
+++ routes/checkout.js
@@ -7,30 +7,23 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 
 checkout.post('/', async (req, res) => {
+    const products = req.body;
+
     try {
+        //create a Stripe session
         const session = await stripe.checkout.sessions.create({
             billing_address_collection: 'auto',
             shipping_address_collection: {
                 allowed_countries: ['GB', 'US']
             },
             payment_method_types: ['card'],
-            line_items: [
-                {
-                    price_data: {
-                        currency: 'usd',
-                        product_data: {
-                            name: 'T-shirt'
-                        },
-                        unit_amount: 2000
-                    },
-                    quantity: 1
-                }
-            ],
+            line_items: products,
             mode: 'payment',
             success_url: 'http://localhost:3000/account?success=true',
             cancel_url: 'http://localhost:3000/account?canceled=true'
         });
 
+        //send back the session id
         res.json({
             id: session.id
         })
