5edb80ff00f41b6c76d7c2d1cbd3fb4165d803c0
Cristian <cmioveanu@gmail.com>
2021-07-02 17:25:05 +0100

    remove custom cors

--- server.js
+++ server.js
@@ -9,31 +9,6 @@ app.use(express.json({
 }));
 
 
-
-// Access headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://full-stack-e-commerce-frontend.herokuapp.com');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
-
-
-
-
 /* Authentication */
 const dbConfig = require('./config/db');
 const { Pool } = require('pg');
