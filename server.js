c8ec67522071a3c826a66f1481a37038a00acd77
Cristian <cmioveanu@gmail.com>
2021-07-02 17:05:35 +0100

    add port 3000

--- server.js
+++ server.js
@@ -54,5 +54,5 @@ app.use('/api/checkout', checkoutRouter);
 
 
 /* Start Server */
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
