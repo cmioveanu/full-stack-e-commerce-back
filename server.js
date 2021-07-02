001a8a59a19e521b9bfb61c16b6fd877bb150653
Cristian <cmioveanu@gmail.com>
2021-07-02 17:19:23 +0100

    update access header

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json({
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://full-stack-e-commerce-frontend.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Origin', 'full-stack-e-commerce-frontend.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
