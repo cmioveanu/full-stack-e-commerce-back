8372be89f57e392c3a02111155a448efaccf0681
Cristian <cmioveanu@gmail.com>
2021-07-02 17:34:50 +0100

    remove reject unathurized

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
 const path = require('path');
 require('dotenv').config({path: '../.env'});
 
+
 const dbConfig = {
     user:     process.env.DB_USER,
     password: process.env.DB_PASS,
     host:     process.env.DB_HOST,
     database: process.env.DB,
-    port:     process.env.DB_PORT,
-    rejectUnauthorized: false
+    port:     process.env.DB_PORT
 };
+
 
 module.exports = dbConfig;
